847b7facb331599803d4924e7cbdd6b47f53d456
omar <elkadi.omar.oe@gmail.com>
2022-01-23 22:24:05 +0200

    fix vulnerabilities trim-newline

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
 // webpack.config.js
 export const module = {
-    rules: [
-        {
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            options: {
-                appendTsSuffixTo: [/\.vue$/],
-            },
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.vue$/,
-            loader: 'vue-loader',
-        },
-    ],
-    plugins: [
-        // ...
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('production')
-        })
-      ]
-};
+  rules: [
+    {
+      test: /\.tsx?$/,
+      loader: 'ts-loader',
+      options: {
+        appendTsSuffixTo: [/\.vue$/]
+      },
+      exclude: /node_modules/
+    },
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }
+  ]
+}
